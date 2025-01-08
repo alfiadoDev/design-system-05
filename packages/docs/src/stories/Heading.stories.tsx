e503d0c9b84d6956e85ef4b72d8f9268337dd073
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-01-08 20:28:11 +0200

    component: heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,26 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Title',
+  }
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1'
+  },
+  parameters: {
+    docs: {
+      descriptions: {
+        story: 'Por padrao o heading sempre sera um `h2`, mas podemos alterar isso com a propriedade `as`',
+      }
+    }
+  }
+}
