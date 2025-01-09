b825eea59d0d054d9fddfd1bde83c7bc5f66d1e7
Alfiado Constantino <alfiado.ngana@xit2s.com>
2025-01-09 22:19:01 +0200

    controles nos components

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -10,11 +10,25 @@ export default {
     layout: 'centered',
   },
   args: {
-    children: 'Send'
+    children: 'Send',
+    variant: 'primary',
+    size: 'md',
+    disabled: false,
   },
   argTypes: {
     onClick: {
       action: 'click'
+    },
+    variant: {
+      options: ['primary', 'secondary', 'tertiary'],
+      control: { type: 'inline-radio' }
+    },
+    size: {
+      options: ['sm', 'md'],
+      control: { type: 'inline-radio' }
+    },
+    disabled: {
+      control: { type: 'boolean' }
     }
   }
 } as Meta<ButtonProps>
